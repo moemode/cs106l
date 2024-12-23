870ada1bb595743784c522e9bdeb73517b808f3f
moemode <pfeffer.moritz@gmail.com>
2024-12-23 09:12:17 +0000

    Solve a2

--- assign2/main.cpp
+++ assign2/main.cpp
@@ -1,6 +1,7 @@
 /*
  * CS106L Assignment 2: Marriage Pact
- * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob Roberts-Baca.
+ * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob
+ * Roberts-Baca.
  *
  * Welcome to Assignment 2 of CS106L! Please complete each STUDENT TODO
  * in this file. You do not need to modify any other files.
@@ -13,50 +14,85 @@
 #include <set>
 #include <string>
 #include <unordered_set>
+#include <cstdlib>
+#include <ctime>
 
 #include "utils.h"
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+std::string kYourName = "King Kong"; // Don't forget to change this!
 
 /**
- * Takes in a file name and returns a set containing all of the applicant names as a set.
+ * Takes in a file name and returns a set containing all of the applicant names
+ * as a set.
  *
  * @param filename  The name of the file to read.
  *                  Each line of the file will be a single applicant's name.
  * @returns         A set of all applicant names read from the file.
  *
- * @remark Feel free to change the return type of this function (and the function
- * below it) to use a `std::unordered_set` instead. If you do so, make sure
- * to also change the corresponding functions in `utils.h`.
+ * @remark Feel free to change the return type of this function (and the
+ * function below it) to use a `std::unordered_set` instead. If you do so, make
+ * sure to also change the corresponding functions in `utils.h`.
  */
 std::set<std::string> get_applicants(std::string filename) {
-  // STUDENT TODO: Implement this function.
+  std::ifstream file(filename);
+  std::string name;
+  std::set<std::string> applicants;
+  while (std::getline(file, name) && !name.empty()) {
+    applicants.insert(name);
+  }
+  return applicants;
+}
+
+std::pair<char, char> get_initials(std::string name) {
+  size_t space_pos = name.find(' ');
+  if (space_pos != std::string::npos) {
+    return {name[0], name[space_pos + 1]};
+  }
+  return {'\0', '\0'};
 }
 
 /**
  * Takes in a set of student names by reference and returns a queue of names
  * that match the given student name.
  *
- * @param name      The returned queue of names should have the same initials as this name.
+ * @param name      The returned queue of names should have the same initials as
+ * this name.
  * @param students  The set of student names.
  * @return          A queue containing pointers to each matching name.
  */
-std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
-  // STUDENT TODO: Implement this function.
+std::queue<const std::string *> find_matches(std::string name,
+                                             std::set<std::string> &students) {
+  std::queue<const std::string *> matches;
+  std::pair<char, char> initials = get_initials(name);
+  for (const std::string &student : students) {
+    if (initials == get_initials(student)) {
+      matches.push(&student);
+    }
+  }
+  return matches;
 }
 
 /**
- * Takes in a queue of pointers to possible matches and determines the one true match!
+ * Takes in a queue of pointers to possible matches and determines the one true
+ * match!
  *
- * You can implement this function however you'd like, but try to do something a bit
- * more complicated than a simple `pop()`.
+ * You can implement this function however you'd like, but try to do something a
+ * bit more complicated than a simple `pop()`.
  *
  * @param matches The queue of possible matches.
  * @return        Your magical one true love.
  *                Will return "NO MATCHES FOUND." if `matches` is empty.
  */
-std::string get_match(std::queue<const std::string*>& matches) {
-  // STUDENT TODO: Implement this function.
+std::string get_match(std::queue<const std::string *> &matches) {
+  if (matches.empty()) {
+    return "NO MATCHES FOUND.";
+  }
+  std::srand(std::time(nullptr));
+  int random_index = std::rand() % matches.size();
+  for (int i = 0; i < random_index; ++i) {
+    matches.pop();
+  }
+  return *matches.front();
 }
 
 /* #### Please don't modify this call to the autograder! #### */
